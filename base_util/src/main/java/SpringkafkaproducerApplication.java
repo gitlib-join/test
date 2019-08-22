ebea05e7c1cf468e9fa7fb5756fb80e78355ae34
gitlib-join <49745514+gitlib-join@users.noreply.github.com>
2019-08-22 16:09:53 +0800

    Update SpringkafkaproducerApplication.java

--- base_util/src/main/java/SpringkafkaproducerApplication.java
+++ base_util/src/main/java/SpringkafkaproducerApplication.java
@@ -8,7 +8,7 @@ public class SpringkafkaproducerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringkafkaproducerApplication.class, args);
-		//test11
+		//test111
     }
 
 
