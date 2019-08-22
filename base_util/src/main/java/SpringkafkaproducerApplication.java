59f04b68fadaf4e31a23e85c9c7369cab572bd93
mf@jfpal.com <f369369>
2019-08-22 15:36:45 +0800

    18.03.01

--- base_util/src/main/java/SpringkafkaproducerApplication.java
+++ base_util/src/main/java/SpringkafkaproducerApplication.java
@@ -8,5 +8,8 @@ public class SpringkafkaproducerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringkafkaproducerApplication.class, args);
+		//test
     }
+
+
 }
